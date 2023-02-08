260c62847ab2f42414a97b26f3c663196db4a004
Larissa Corrẽa <larimscorrea@gmail.com>
2023-02-07 22:25:15 -0300

    others dones

---
+++ day01.js
@@ -0,0 +1,7 @@
+// Challenge 01: wanna the name and the age of the user and writting that's infos at friendly's shape.
+var name = prompt('Inform your name', 'Write here');
+var age = prompt('Inform the age', 'Write here');
+
+alert(`Welcome ${name}!!! You have ${age} years old.`);
+
+// Comando clear() limpa o console.
---
+++ day02.js
@@ -0,0 +1,7 @@
+// Challenge 01: wanna the mathematic's operations.
+var number01 = prompt('Inform the first number', 'Write here');
+var number02 = prompt('Inform the second number', 'Write here');
+
+var media = (number01 + number02) / 2;
+
+alert(`The result of the media was ${media}`);
