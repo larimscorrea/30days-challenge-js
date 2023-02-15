a4e6b65c6429c57c6b0c94bab832cbd283e60910
Larissa Corrẽa <larimscorrea@gmail.com>
2023-02-15 09:34:07 -0300

    10day

---
+++ day09.js
@@ -0,0 +1,6 @@
+var continuar = confirm("Deseja continuar a operação?");
+if(continuar == true){
+    console.log("Você clicou no botão OK. Vamos continuar a operação.");
+    }else{
+    console.log("Você clicou no botão CANCELAR. Vamos cancelar a operação.");
+}
