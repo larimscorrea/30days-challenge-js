d922a02cfd94fa37ad4c5bfff5edaeabc3b2be3b
Larissa Corrẽa <larimscorrea@gmail.com>
2023-02-19 20:02:04 -0300

    14day

---
+++ day14.js
@@ -0,0 +1,17 @@
+var nome_array = [ valor1 , valor2 , valor3 , valor 4 ,
+    ...];
+    var alunos = ["Maria" , "Rose" , "Claudio" , "Renata" ,
+    "Fernando"];
+
+    console.log(alunos[0]);
+    console.log(alunos[1]);
+    console.log(alunos[2]);
+    console.log(alunos[3]);
+    console.log(alunos[4]);
+
+    var contador = 0;
+    while(contador<=4){
+    console.log(alunos[contador]);
+    contador++;
+    }
+    
